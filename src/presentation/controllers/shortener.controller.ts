ecb0de928959b9d7ee72b08d2d5f555d0e9f56b6
Leonardo S <leon.sdsilva@gmail.com>
2023-08-01 12:35:26 -0300

    feat: ensure ShortenerController.handle return 500 if urlShortener.run throws

--- src/presentation/controllers/shortener.controller.ts
+++ src/presentation/controllers/shortener.controller.ts
@@ -1,8 +1,11 @@
-import { Controller } from '../protocols/controller.protocol';
-import { HttpRequest, HttpResponse } from '../protocols/http.protocol';
-import { ShortenerDto } from '../protocols/shortener-dto.protocol';
-import { InvalidParamError, MissingParamError } from '../errors';
-import { UrlValidator } from '../protocols/url-validator.protocol';
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+  ShortenerDto,
+  UrlValidator,
+} from './shortener.protocol';
+import { InvalidParamError, MissingParamError, ServerError } from '../errors';
 import { UrlShortener } from '../../domain/usecases/url-shortener.usecase';
 
 export class ShortenerController implements Controller {
@@ -12,29 +15,36 @@ export class ShortenerController implements Controller {
   ) {}
 
   async handle(httpRequest: HttpRequest<ShortenerDto>): Promise<HttpResponse> {
-    const url = httpRequest.body?.url ?? '';
-    if (!url) {
+    try {
+      const url = httpRequest.body?.url ?? '';
+      if (!url) {
+        return {
+          statusCode: 400,
+          body: new MissingParamError('url'),
+        };
+      }
+
+      const isValid = this.urlValidator.isValid(url);
+      if (!isValid) {
+        return {
+          statusCode: 400,
+          body: new InvalidParamError('url'),
+        };
+      }
+
+      const urlShortener = this.urlShortener.run(url);
+
       return {
-        statusCode: 400,
-        body: new MissingParamError('url'),
+        statusCode: 200,
+        body: {
+          urlShortener,
+        },
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError(),
       };
     }
-
-    const isValid = this.urlValidator.isValid(url);
-    if (!isValid) {
-      return {
-        statusCode: 400,
-        body: new InvalidParamError('url'),
-      };
-    }
-
-    const urlShortener = this.urlShortener.run(url);
-
-    return {
-      statusCode: 200,
-      body: {
-        urlShortener,
-      },
-    };
   }
 }
