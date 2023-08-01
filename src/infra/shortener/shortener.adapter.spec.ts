4b5d93c153c23f42eb59f16009b38962420d6620
Leonardo S <leon.sdsilva@gmail.com>
2023-08-01 16:40:26 -0300

    test: ensure Shortener Adapter throw if nanoid failed

---
+++ src/infra/shortener/shortener.adapter.spec.ts
@@ -0,0 +1,28 @@
+import * as nanoidModule from 'nanoid';
+
+import { ShortenerAdapter } from './shortener.adapter';
+import { Shortener } from '../../data/protocols/shortener.protocol';
+
+jest.mock('nanoid', () => {
+  return {
+    __esModule: true,
+    nanoid: () => '123456',
+  };
+});
+
+const makeSut = (): Shortener => {
+  return new ShortenerAdapter();
+};
+
+describe('Shortener Adapter', () => {
+  test('Should throw if shorten failed', async () => {
+    const sut = makeSut();
+    const size = 6;
+    jest.spyOn(nanoidModule, 'nanoid').mockImplementationOnce(() => {
+      throw new Error();
+    });
+    const fn = () => sut.shorten(size);
+
+    expect(fn).toThrow(Error);
+  });
+});
