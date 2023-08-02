6176113f93cad64948a6d8bbf249f3cd77de837a
Leonardo S <leon.sdsilva@gmail.com>
2023-08-02 18:12:41 -0300

    test: ensure MapHelper return shortened with domain

--- src/data/usecases/helpers/map.helper.spec.ts
+++ src/data/usecases/helpers/map.helper.spec.ts
@@ -2,8 +2,33 @@ import * as sut from './map.helper';
 import { UrlShortenerModel } from '../../../domain/models/url-shortener.model';
 import { env } from '../../../main/config/env.config';
 
+jest.mock('../../../main/config/env.config', () => {
+  return {
+    env: {
+      port: Number(process.env.PORT) || 3000,
+      domain: process.env.DOMAIN ?? 'localhost',
+    },
+  };
+});
+
 describe('Map Helper', () => {
   test('Should return shortened with domain', async () => {
+    env.port = null;
+    const url: UrlShortenerModel = {
+      id: 'valid_id',
+      original: 'original_url',
+      shortened: 'shortened_url',
+    };
+    const mapped = sut.mapper(url);
+
+    expect(mapped).toBeTruthy();
+    expect(mapped.id).toBeTruthy();
+    expect(mapped.original).toBe(url.original);
+    expect(mapped.shortened).toBe(`${env.domain}/${url.shortened}`);
+  });
+
+  test('Should return shortened with domain and port', async () => {
+    env.port = 3000;
     const url: UrlShortenerModel = {
       id: 'valid_id',
       original: 'original_url',
--- src/main/config/env.config.ts
+++ src/main/config/env.config.ts
@@ -1,5 +1,11 @@
-export const env = {
-  port: process.env.PORT ?? 3000,
+interface Env {
+  port: number | null;
+  domain: string;
+  mongoUrl: string;
+}
+
+export const env: Env = {
+  port: Number(process.env.PORT) || 3000,
   domain: process.env.DOMAIN ?? 'localhost',
   mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/url-shortener',
 };
