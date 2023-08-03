ae3de46635b8fd0e8406882c722e4202f39b757e
Leonardo S <leon.sdsilva@gmail.com>
2023-08-03 12:13:45 -0300

    refactor: ensure correct code validation

--- src/presentation/controllers/redirect-to-original-url.controller.spec.ts
+++ src/presentation/controllers/redirect-to-original-url.controller.spec.ts
@@ -1,12 +1,35 @@
+import { CodeValidator } from '../protocols/code-validator.protocol';
 import { RedirectToOriginalUrlController } from './redirect-to-original-url.controller';
 
-const makeSut = (): RedirectToOriginalUrlController => {
-  return new RedirectToOriginalUrlController();
+const makeCodeValidatorAdapterStub = (): CodeValidator => {
+  class CodeValidatorAdapterStub implements CodeValidator {
+    isValid(code: string): boolean {
+      return true;
+    }
+  }
+
+  return new CodeValidatorAdapterStub();
+};
+
+interface SutTypes {
+  sut: RedirectToOriginalUrlController;
+  codeValidatorStub: CodeValidator;
+}
+
+const makeSut = (): SutTypes => {
+  const codeValidatorStub = makeCodeValidatorAdapterStub();
+  const sut = new RedirectToOriginalUrlController(codeValidatorStub);
+
+  return {
+    sut,
+    codeValidatorStub,
+  };
 };
 
 describe('RedirectToOriginalUrl Controller', () => {
   test('Should return 400 if incorrect path param', async () => {
-    const sut = makeSut();
+    const { sut, codeValidatorStub } = makeSut();
+    jest.spyOn(codeValidatorStub, 'isValid').mockReturnValueOnce(false);
     const httpRequest = {
       params: {
         code: 'invalid_code',
--- src/presentation/controllers/redirect-to-original-url.controller.ts
+++ src/presentation/controllers/redirect-to-original-url.controller.ts
@@ -1,13 +1,22 @@
-import { serverError } from '../helpers/http.helper';
+import { InvalidParamError } from '../errors';
+import { badRequest, serverError } from '../helpers/http.helper';
+import { CodeValidator } from '../protocols/code-validator.protocol';
 import { Controller, HttpRequest, HttpResponse } from './shortener.protocol';
 
 export class RedirectToOriginalUrlController implements Controller {
+  constructor(private readonly codeValidator: CodeValidator) {}
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { code } = httpRequest.params;
+      const isValid = this.codeValidator.isValid(code);
+
+      if (!isValid) {
+        return badRequest(new InvalidParamError('code'));
+      }
 
       return {
-        statusCode: 400,
+        statusCode: 200,
         body: {},
       };
     } catch (error) {
