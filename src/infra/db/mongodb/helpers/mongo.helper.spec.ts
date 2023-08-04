e45b5777d3b833fedc4e748366e637b0792aa7bd
Leonardo S <leon.sdsilva@gmail.com>
2023-08-04 13:35:07 -0300

    test: ensure reconnection if needed before getCollection

---
+++ src/infra/db/mongodb/helpers/mongo.helper.spec.ts
@@ -0,0 +1,21 @@
+import { MongoHelper as sut } from './mongo.helper';
+
+describe('Mongo Helper', () => {
+  beforeAll(async () => {
+    const uri = process.env.MONGO_URL ?? '';
+    await sut.connect(uri);
+  });
+
+  afterAll(async () => {
+    await sut.disconnect();
+  });
+
+  test('Should reconnection if needed before getCollection', async () => {
+    let urlCollection = await sut.getCollection('urls');
+    expect(urlCollection).toBeTruthy();
+
+    await sut.disconnect();
+    urlCollection = await sut.getCollection('urls');
+    expect(urlCollection).toBeTruthy();
+  });
+});
